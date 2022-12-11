33be3f55c65f2d0edd883f2bf29e3e819264af8f
Keunoh <rmsdhcjswo1@gmail.com>
2022-12-11 23:43:51 +0900

    patch: lombok

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import hello.core.AppConfig;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,9 +14,6 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-
-
-    //생성자가 딱! 1개일 경우에는 @Autowired를 생략할 수 있음
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
